9f8eb07f6d80e68da5e9b375205261c50689e1bd
jaybyrd3 <123055625+jaybyrd3@users.noreply.github.com>
2023-02-01 19:52:26 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,22 +1,21 @@
+import uvicorn
+import json
+import requests
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware 
-import uvicorn 
 
-app = FastAPI() 
+app = FastAPI()
 
-origins = ['*']
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-) 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello from Fastapi"} 
+    return "PISS OFF"
+
+
+@app.get("/get_link")
+def get_link(link):
+    r = requests.get(link)
+    return(r.json())
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8000)
